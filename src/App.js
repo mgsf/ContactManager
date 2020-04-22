b8e7686fdad6a8a0103e33683a09a41cee30434e
Gayan <gayanf@gmail.com>
2020-04-22 13:46:49 +0530

    Initial working project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+
+        return (
+            <div className="App">
+                <Header branding="Contact Manager"/>
+                <div className="container">
+                    <Contact name="John Doe"
+                             email="jdoe@gmail.com"
+                             phone="555-555-555"/>
+                    <Contact name="Gayan Fonseka"
+                             email="mgsf@gmail.com"
+                             phone="333-555-555"/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
