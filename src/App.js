58775c2092b1a0c7703aeee93b88471f65fcaac8
Gayan <gayanf@gmail.com>
2020-04-23 21:12:44 +0530

    Delete with Context API & Reducers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 
+import { Provider } from './context';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -10,12 +12,14 @@ class App extends Component {
     render() {
 
         return (
+            <Provider>
             <div className="App">
                 <Header branding="Contact Manager"/>
                 <div className="container">
                     <Contacts />
                 </div>
             </div>
+            </Provider>
         );
     }
 }
