6cfa2c1af2c6feeb997eeebd9e87e90e439d570f
Gayan <gayanf@gmail.com>
2020-04-23 15:43:22 +0530

    Changing state -basic (No Context API or Reducer)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,26 @@ class Contact extends Component {
         this.setState({ showContactInfo: !this.state.showContactInfo})
     };
 
+    onDeleteClick = () => {
+        this.props.deleteClickHandler();
+    }
+
     render() {
         const { name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state;
 
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down" /> </h4>
+                <h4>{name} <i onClick={this.onShowClick}
+                              className="fas fa-sort-down"
+                              style={{ cursor: 'pointer'}}
+                            />
+                            <i className="fas fa-times"
+                               style={{ cursor: 'pointer',
+                               float:'right', color:'red'}}
+                               onClick={this.onDeleteClick}
+                            />
+                </h4>
                 {showContactInfo ? (
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
@@ -29,6 +42,7 @@ class Contact extends Component {
 
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
